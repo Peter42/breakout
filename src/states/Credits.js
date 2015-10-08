70b7c539d986fc28d220afef823e2e0bbb6f4814
Philipp <anderschwiedu@gmail.com>
2015-10-09 01:18:35 +0200

    First Credits

--- src/states/Credits.js
+++ src/states/Credits.js
@@ -5,9 +5,54 @@ doodleBreakout.Credits = function( game ){
 };
 
 doodleBreakout.Credits.prototype = {
+
+    creditStrings: [
+        "",
+        "Developed by Magic WG and Lara",
+        "Graphics by Lara",
+        "Sounds by Marcel and Lina",
+        "Infrastructure by Philipp",
+        "",
+        "Libraries:",
+        "Phaser.js"
+
+    ].reverse(),
+
     create: function(){
+
+        this.elements = this.game.add.group();
+
+        this.game.physics.startSystem(Phaser.Physics.ARCADE);
 
         var title = this.game.add.bitmapText(this.game.width / 2, 10, 'larafont', 'Credits',64);
         title.anchor.setTo(0.5, 0);
+        this.game.physics.enable(title, Phaser.Physics.ARCADE);
+        title.body.immovable = true;
+
+        this.elements.add(title);
+
+        this.timer = this.game.time.create(false);
+        this.timer.loop(1500, this.next, this);
+        this.timer.start();
+
+        this.next();
+    },
+
+    next: function() {
+        var credit = this.game.add.bitmapText(this.game.width / 2, this.game.height, 'larafont', this.creditStrings.pop() ,48);
+        credit.anchor.setTo(0.5, 0);
+        this.game.physics.enable(credit, Phaser.Physics.ARCADE);
+        credit.body.velocity.setTo(0,-100);
+
+        this.elements.add(credit);
+
+    },
+
+    update:function(){
+        this.game.physics.arcade.collide(this.elements, this.elements, function(a,b) {
+            a.body.velocity.setTo(0);
+            b.body.velocity.setTo(0);
+        });
     }
+
 };
