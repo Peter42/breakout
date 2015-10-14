79b2487db2c69478e88d1a264a4ed9ed261a4837
Philipp Stehle <philipp.stehle@sap.com>
2015-10-14 16:17:32 +0200

    Scale live icon permanent

--- src/sprites/Live.js
+++ src/sprites/Live.js
@@ -3,12 +3,7 @@ var doodleBreakout = doodleBreakout || {};
 doodleBreakout.Live = function (game, x, y) {
 
     //  We call the Phaser.Sprite passing in the game reference
-    Phaser.Sprite.call(this, game, x, y, 'live');
-
-    this.scale.setTo( 0.3, 0.3 );
-
-    //game.physics.enable(this, Phaser.Physics.ARCADE);
-
+    Phaser.Sprite.call(this, game, x, y, 'Live');
 };
 
 doodleBreakout.Live.prototype = Object.create(Phaser.Sprite.prototype);
