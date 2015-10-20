0b45d21143f99a692144044aa6aaee09bc6a8fe0
Philipp <anderschwiedu@gmail.com>
2015-10-20 16:31:13 +0200

    Display Highscores

--- src/states/Highscores.js
+++ src/states/Highscores.js
@@ -15,4 +15,17 @@ doodleBreakout.Highscores.prototype.create = function(){
     var title = this.game.add.bitmapText(this.game.width / 2, 10, 'larafont', 'Highscores',64);
     title.anchor.setTo(0.5, 0);
 
+    var scores = doodleBreakout.ScoresManager.getHighscores();
+
+    for(var i in scores){
+        this._addScore(scores[i], 45 * i + 120);
+    }
+
+};
+
+doodleBreakout.Highscores.prototype._addScore = function(score, y) {
+    var text = this.game.add.bitmapText(this.world.centerX - 10, y, 'larafont', score.name,32);
+    text.anchor.set(1,0);
+    this.game.add.bitmapText(this.world.centerX + 10, y, 'larafont', score.score + "",32);
+
 };
