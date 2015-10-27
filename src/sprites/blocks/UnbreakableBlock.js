91518c7acc2aa5329003410364b6ced034961e19
gasserus <gasserus@gmail.com>
2015-10-27 21:19:47 +0100

    add unbreakable block

---
+++ src/sprites/blocks/UnbreakableBlock.js
@@ -0,0 +1,16 @@
+var doodleBreakout = doodleBreakout || {};
+
+doodleBreakout.UnbreakableBlock = function ( game, x, y ) {
+    doodleBreakout.AbstactBlock.call(this, game, x, y, 'block05');
+};
+
+doodleBreakout.UnbreakableBlock.prototype = Object.create(doodleBreakout.AbstactBlock.prototype);
+doodleBreakout.UnbreakableBlock.prototype.constructor = doodleBreakout.UnbreakableBlock;
+
+doodleBreakout.UnbreakableBlock.prototype.hit = function( ball ){
+    if( ball.isThunderball ){
+        this.kill();
+        return true;
+    }
+    return false;
+};
--- src/states/Preloader.js
+++ src/states/Preloader.js
@@ -24,6 +24,7 @@ doodleBreakout.Preloader.prototype = {
         this.game.load.spritesheet('block02', 'assets/images/block02.png', 50, 16);
         this.game.load.spritesheet('block03', 'assets/images/block03.png', 50, 16);
         this.game.load.spritesheet('block04', 'assets/images/block04.png', 50, 16);
+        this.game.load.spritesheet('block05', 'assets/images/blockfest.png', 50, 16);
         this.game.load.image('hintergrund', 'assets/images/hintergrund.png');
         this.game.load.image('easteregg', 'assets/images/easteregg.png');
         this.game.load.image('coin', 'assets/images/coin.png');
