bf787fd81f8a0bd614d0057b4f2c62cb020db075
Philipp <anderschwiedu@gmail.com>
2015-10-29 20:10:30 +0100

    Add Polyfill for Array.find for Internet Explorer support.

--- src/app.js
+++ src/app.js
@@ -16,3 +16,31 @@ window.onload = function(){
     game.state.add( 'GameOver'    , doodleBreakout.GameOver );
     game.state.add( 'Boot'          , doodleBreakout.Boot, true );
 };
+
+
+// Polyfills
+
+// Array.find
+// https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Global_Objects/Array/find
+if (!Array.prototype.find) {
+    Array.prototype.find = function(predicate) {
+        if (this == null) {
+            throw new TypeError('Array.prototype.find called on null or undefined');
+        }
+        if (typeof predicate !== 'function') {
+            throw new TypeError('predicate must be a function');
+        }
+        var list = Object(this);
+        var length = list.length >>> 0;
+        var thisArg = arguments[1];
+        var value;
+
+        for (var i = 0; i < length; i++) {
+            value = list[i];
+            if (predicate.call(thisArg, value, i, list)) {
+                return value;
+            }
+        }
+        return undefined;
+    };
+}
